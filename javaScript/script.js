1c2a69a777e6e2cf158cc503028159407a9fe1e2
Wittchen <swittche@ipb-halle.de>
2024-06-07 15:04:20 +0200

    phase 2: content to page 1 added

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,36 +1,36 @@
 //JQUERY
 
-$(document).ready(function () {
-    $(".btn1").click(function () {
-        $.ajax({
-            url: "./html/page1.html",
-            method: "GET",
-            success: function (response) {
-                $(".content").html(response);
-            },
-            error: function () {
-                alert("Fehler beim Laden der Seite");
-            }
-        });
-    });
-});
+// $(document).ready(function () {
+//     $(".btn1").click(function () {
+//         $.ajax({
+//             url: "./html/page1.html",
+//             method: "GET",
+//             success: function (response) {
+//                 $(".content").html(response);
+//             },
+//             error: function () {
+//                 alert("Fehler beim Laden der Seite");
+//             }
+//         });
+//     });
+// });
 
 
 // JAVASCRIPT
-// let btn1 = document.querySelector(".btn1");
+let btn1 = document.querySelector(".btn1");
 
 
-// function changePage() {
+function changePage() {
 
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", "./html/page1.html", true);
-//     xhr.onreadystatechange = () => {
-//         if (xhr.readyState == 4 && xhr.status == 200) {
-//             let pageContent = xhr.responseText;
-//             let page = document.querySelector(".content");
-//             page.innerHTML = pageContent;
-//         }
-//     }
-//     xhr.send();
-// }
-// btn1.onclick = changePage;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "./html/page1.html", true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let pageContent = xhr.responseText;
+            let page = document.querySelector(".content");
+            page.innerHTML = pageContent;
+        }
+    }
+    xhr.send();
+}
+btn1.onclick = changePage;
